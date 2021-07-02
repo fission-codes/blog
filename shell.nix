2ea7003d3f40f74c75bb51b3443fdd6ee2148cc2
James Walker <walkah@walkah.net>
2021-07-02 13:48:26 -0400

    switch to nix-direnv over lorri
    - lorri hides shellHook output
    - also no need to be on unstable nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,11 +1,12 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs { };
+  yarn = pkgs.yarn.override { nodejs = pkgs.nodejs-16_x; };
 in pkgs.mkShell {
   name = "blog";
-  buildInputs = with pkgs; [ nodejs yarn ];
+  buildInputs = with pkgs; [ nodejs-16_x yarn ];
 
   shellHook = ''
-    ${pkgs.yarn}/bin/yarn install
+    ${yarn}/bin/yarn install
   '';
 }
