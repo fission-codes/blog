8b1894225399c454603628e1c7dbda5a591eb98e
James Walker <walkah@walkah.net>
2021-08-19 11:06:03 -0400

    bump versions, set build target

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "es2020",
+  },
   plugins: [reactRefresh()],
 });
