7906244e3344b941bd118d17b6e5a759cd9fbf84
nichoth <nichoth@gmail.com>
2021-10-18 15:27:08 -0700

    rm messy comments

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -3,79 +3,21 @@ import React, {
   FunctionComponent,
   useState
 } from 'react';
-// import { useForm } from "react-hook-form";
 import Layout from "../components/Layout";
 import { useWebnative } from "../context/webnative";
 import * as wn from "webnative";
 import { FilePath } from "webnative/path";
 import { Feed } from "../utils/feed";
 
-// type Inputs = {
-//   title: string;
-//   content: string;
-//   image: string;
-// };
-
 type EditorProps = {
   feed: Feed
 }
 
 const Editor: FunctionComponent<EditorProps> = ({ feed }) => {
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   formState: { errors },
-  // } = useForm<Inputs>();
-
   const _wn = useWebnative()
   const { fs } = _wn
 
   const [previewImage, setPreviewImage] = useState<string | null>(null);
-
-
-
-  // ???
-  // Argument of type 'string' is not assignable to parameter of type 'Blob'.
-  // this typescript error is only when using the react-hook-form dependency,
-  // not with a standard event handler function
-
-  // const onSubmit = handleSubmit(async (data) => {
-  //   if (!fs || !fs.appPath) return
-
-  //   console.log('**on submit**', data)
-
-    // data.image is a `FileList` object
-    // const imageFile = data.image[0]
-    // console.log('**image', imageFile)
-
-    // why does this give a ts error?
-    // -----------------------------------------------------
-    // const reader = new FileReader()
-
-    // reader.onloadend = () => {
-    //   console.log('reader.result', reader.result)
-    // }
-
-    // ???
-    // Argument of type 'string' is not assignable to parameter of type 'Blob'.
-    // reader.readAsDataURL(imageFile)
-    // -----------------------------------------------------
-
-
-    // feed.addItem({
-    //   // TODO -- how to get id?
-    //   // could take the hash of the post (without id attribute), then
-    //   //   add `id: <hash>`
-    //   id: '1',
-    //   content_text: data.content,
-    //   title: data.title
-    // })
-
-    // const feedPath = fs.appPath(wn.path.file("feed.json"));
-    // fs.write(feedPath as FilePath, feed.toString())
-    //   // TODO -- should show resolving status while we publish
-    //   .then(() => fs.publish())
-  // });
 
   interface FeedData {
     title: string;
@@ -100,11 +42,7 @@ const Editor: FunctionComponent<EditorProps> = ({ feed }) => {
       // could take the hash of the post (without id attribute), then
       //   add `id: <hash>`
       id: '1',
-
-      // how can we record just an address for the image?
-      // the image should be already written to disk when this is called
       image: img,
-
       content_text: data.content,
       title: data.title
     })
@@ -118,16 +56,14 @@ const Editor: FunctionComponent<EditorProps> = ({ feed }) => {
 
   // -----------------------------------------------------------------------
 
-  function getNameFromFile (file:File) {
-    const url = URL.createObjectURL(file);
-    // blob:http://localhost:3000/83507733-bfb8-42dd-ac10-638e2c28c776
-    var slug = url.split('/').pop()
-    var ext = file.name.split('.').pop()
-    var fileName = slug + '.' + ext
-    return fileName
-  }
-
-  // const content = await fs.cat(wn.path.file("public", "some", "file"))
+  // function getNameFromFile (file:File) {
+  //   const url = URL.createObjectURL(file);
+  //   // blob:http://localhost:3000/83507733-bfb8-42dd-ac10-638e2c28c776
+  //   var slug = url.split('/').pop()
+  //   var ext = file.name.split('.').pop()
+  //   var fileName = slug + '.' + ext
+  //   return fileName
+  // }
 
   const submitter = (ev: BaseSyntheticEvent) => {
     if (!(fs && fs.appPath)) return
@@ -135,45 +71,20 @@ const Editor: FunctionComponent<EditorProps> = ({ feed }) => {
     const image:File = ev.target.elements.image.files[0]
     console.log('**image', image)
 
-    var fileName = getNameFromFile(image)
+    // var fileName = getNameFromFile(image)
 
     const data = ['title', 'content'].reduce((acc: any, k) => {
       acc[k] = ev.target.elements[k].value
       return acc
     }, {})
 
-    // first save the image,
-    // then update the feed and save the feed
-    // (this is a two step process, not atomic)
-    // fs.write(fs.appPath(wn.path.file(fileName)), image)
-    //   .then(() => updateFeed(data, fileName))
-
-    // if we save a file to ipfs, what do we use as the URL in the
-    // img tag source?
-
-    // need to take the filename in the message, read it from wn.fs,
-    // then do `createObjectURL` on the file object
-
-
 
     const reader = new FileReader()
     reader.onloadend = () => {
       console.log('load end')
       updateFeed(data, reader.result as string)
-      // onSubmit(ev)
-      // updateFeed(data, reader.result as ArrayBuffer)
     }
     reader.readAsDataURL(image)  // this gives us base64
-
-    // const url = URL.createObjectURL(image);
-    // console.log('url', url)
-
-    // like filereader, but more direct
-    // image.arrayBuffer()
-    //   .then(buf => {
-    //     console.log('*buf*', buf)
-    //     updateFeed(data, buf)
-    //   })
   }
 
   function changer (ev: BaseSyntheticEvent) {
@@ -183,27 +94,6 @@ const Editor: FunctionComponent<EditorProps> = ({ feed }) => {
     const url = URL.createObjectURL(image);
     console.log('*url*', url)
     setPreviewImage(url)
-
-    // var slug = url.split('/').pop()
-    // var ext = image.name.split('.').pop()
-
-    // var fileName = slug + '.' + ext
-
-    // fs.write(fs.appPath(wn.path.file(fileName)), image)
-
-    // console.log(
-    //   'path',
-    //   fs.appPath(),
-    //   slug,
-    //   ext,
-    //   'aaa'
-    // )
-
-    // image.arrayBuffer()
-    //   .then(buf => {
-    //     console.log('*buf*', buf)
-    //     // fs.write(fs.appPath(wn.path.file(fileName)), buf)
-    //   })
   }
 
   // -----------------------------------------------------------------------
--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -2,45 +2,13 @@ import React, { useState, FunctionComponent } from 'react';
 import { Link } from "react-router-dom";
 import Layout from "../components/Layout";
 import { Feed } from "../utils/feed";
-// import { RouteComponentProps } from "react-router-dom";
-// import * as _wn from "webnative";
-// import { path } from 'webnative'
-// import { useWebnative, WebnativeContext } from "../context/webnative";
-
 
 type PostProps = {
   feed: Feed
 }
 
-// function getImg (wn: WebnativeContext, feedItem: Item) {
-//   const { fs } = wn
-//   if (!fs || !fs.appPath) return
-//   console.log('fs', fs)
-//   var fileName = feedItem.image
-//   if (!fileName) return
-//   console.log('filename', fileName)
-//   return fs.cat(fs.appPath(path.file(fileName))).then((content) => {
-//     console.log('content', content)
-//     // if (!content) return ''
-//     var url = URL.createObjectURL(new Blob([content], {type: "image/jpeg"}))
-//     // var url = URL.createObjectURL(content)
-//     // var url = URL.createObjectURL(new Blob([content]))
-//     return url
-//     // return content
-//   })
-// }
-
-// > we encourage using IPFS hashes as the canonical representation for data
-//   * where do you get the ipfs hash?
-
-// need to read the images from wn.fs and do url.create
-// before you put the src in the img tag
-
-// const Posts: FunctionComponent<PostProps & RouteComponentProps<any>> = ({ feed }) => {
-// const Posts: FunctionComponent<RouteComponentProps<PostProps>> = ({ feed }) => {
 const Posts: FunctionComponent<PostProps> = ({ feed }) => {
   console.log('in posts', feed)
-  // const wn = useWebnative()
 
   return (
     <Layout>
@@ -62,9 +30,6 @@ const Posts: FunctionComponent<PostProps> = ({ feed }) => {
           </li>
 
           {feed?.items.map((item, i) => {
-
-            // getImg(wn, item)
-            //   .then((res:string) => console.log('aaaa i', res, i))
 
             return (<li key={i} className="table-row bg-white">
                 <div className="table-cell img-cell">
